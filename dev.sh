02ca755a86f2faf15c88b29a39d334493c2427a8
William A. Wieselquist <ww5@ornl.gov>
2024-11-01 07:53:55 -0400

    Update development init script dev.sh

--- dev.sh
+++ dev.sh
@@ -1,13 +1,24 @@
 if [ ! -e venv ]
 then
+    echo "Setting up virtual environment..."
     virtualenv venv
 fi
 
+echo "Activating virtual environment..."
 source venv/bin/activate
+
+echo "Installing requirements..."
 pip install -r requirements.txt
+
+echo "Installing OLM..."
 pip install --editable .
 
-echo running tests
+echo "Updating data submodule..."
+git submodule update --init data
+pushd data
+git lfs pull .
+popd
 
+echo "Running tests"
 pytest -n 6 .
 
