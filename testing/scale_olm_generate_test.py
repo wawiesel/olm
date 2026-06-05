fb166ed089f32ab1e89c89483fcb99896718e181
Will Wieselquist <william.wieselquist@gmail.com>
2026-06-05 12:25:54 -0400

    Add custom UO2 vector regression test

--- testing/scale_olm_generate_test.py
+++ testing/scale_olm_generate_test.py
@@ -25,3 +25,15 @@ def test_constpower_burndata():
         assert found, "No approximate match found for {} in {}".format(
             t, ",".join(str(e) for e in mid_burnups)
         )
+
+
+def test_mox_ornltm2003_2_uses_custom_uo2_vector():
+    comp = olm.generate.comp.mox_ornltm2003_2(
+        state={"pu239_frac": 70.0, "pu_frac": 5.0},
+        uo2={"iso": {"u235": 4.0, "u238": 96.0}},
+    )
+
+    assert comp["_input"]["uo2"]["iso"]["u235"] == pytest.approx(4.0)
+    assert comp["_input"]["uo2"]["iso"]["u238"] == pytest.approx(96.0)
+    assert comp["hmo2"]["iso"]["u235"] == pytest.approx(3.8)
+    assert comp["hmo2"]["iso"]["u238"] == pytest.approx(91.2)
